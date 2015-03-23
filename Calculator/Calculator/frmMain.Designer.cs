5b5eb4ba758fb46f1c704a3d5deb661c0e085d87
danhkhanh <ronwindy@gmail.com>
2015-03-23 20:52:53 +0700

    bai calculator dau tien

---
+++ Calculator/Calculator/frmMain.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Calculator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.btnChia = new System.Windows.Forms.Button();
+            this.btnBang = new System.Windows.Forms.Button();
+            this.btnThapPhan = new System.Windows.Forms.Button();
+            this.btnAmDuong = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnSqrt = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDisplay.Location = new System.Drawing.Point(4, 9);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(277, 29);
+            this.lblDisplay.TabIndex = 0;
+            this.lblDisplay.Text = "0.";
+            this.lblDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(12, 66);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(30, 30);
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.NhapSo);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(58, 66);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(30, 30);
+            this.btn8.TabIndex = 8;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.NhapSo);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(105, 66);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(30, 30);
+            this.btn9.TabIndex = 9;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.NhapSo);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(12, 111);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(30, 30);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.NhapSo);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(58, 111);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(30, 30);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.NhapSo);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(105, 111);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(30, 30);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.NhapSo);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 161);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(30, 30);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.NhapSo);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(58, 161);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(30, 30);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.NhapSo);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(105, 161);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(30, 30);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.NhapSo);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(12, 208);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(76, 30);
+            this.btn0.TabIndex = 0;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.NhapSo);
+            // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(154, 66);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(30, 30);
+            this.btnCong.TabIndex = 11;
+            this.btnCong.Text = "+";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.NhapPhepToan);
+            // 
+            // btnTru
+            // 
+            this.btnTru.Location = new System.Drawing.Point(154, 111);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(30, 30);
+            this.btnTru.TabIndex = 12;
+            this.btnTru.Text = "-";
+            this.btnTru.UseVisualStyleBackColor = true;
+            this.btnTru.Click += new System.EventHandler(this.btnTru_Click);
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.Location = new System.Drawing.Point(154, 161);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(30, 30);
+            this.btnNhan.TabIndex = 13;
+            this.btnNhan.Text = "*";
+            this.btnNhan.UseVisualStyleBackColor = true;
+            this.btnNhan.Click += new System.EventHandler(this.btnNhan_Click);
+            // 
+            // btnChia
+            // 
+            this.btnChia.Location = new System.Drawing.Point(154, 208);
+            this.btnChia.Name = "btnChia";
+            this.btnChia.Size = new System.Drawing.Size(30, 30);
+            this.btnChia.TabIndex = 14;
+            this.btnChia.Text = "/";
+            this.btnChia.UseVisualStyleBackColor = true;
+            this.btnChia.Click += new System.EventHandler(this.btnChia_Click);
+            // 
+            // btnBang
+            // 
+            this.btnBang.Location = new System.Drawing.Point(245, 161);
+            this.btnBang.Name = "btnBang";
+            this.btnBang.Size = new System.Drawing.Size(33, 77);
+            this.btnBang.TabIndex = 15;
+            this.btnBang.Text = "=";
+            this.btnBang.UseVisualStyleBackColor = true;
+            this.btnBang.Click += new System.EventHandler(this.btnBang_Click);
+            // 
+            // btnThapPhan
+            // 
+            this.btnThapPhan.Location = new System.Drawing.Point(105, 208);
+            this.btnThapPhan.Name = "btnThapPhan";
+            this.btnThapPhan.Size = new System.Drawing.Size(30, 30);
+            this.btnThapPhan.TabIndex = 16;
+            this.btnThapPhan.Text = ".";
+            this.btnThapPhan.UseVisualStyleBackColor = true;
+            this.btnThapPhan.Click += new System.EventHandler(this.btnThapPhan_Click);
+            // 
+            // btnAmDuong
+            // 
+            this.btnAmDuong.Location = new System.Drawing.Point(200, 111);
+            this.btnAmDuong.Name = "btnAmDuong";
+            this.btnAmDuong.Size = new System.Drawing.Size(30, 30);
+            this.btnAmDuong.TabIndex = 17;
+            this.btnAmDuong.Text = "+/-";
+            this.btnAmDuong.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 75);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "C";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSqrt
+            // 
+            this.btnSqrt.Location = new System.Drawing.Point(200, 208);
+            this.btnSqrt.Name = "btnSqrt";
+            this.btnSqrt.Size = new System.Drawing.Size(30, 30);
+            this.btnSqrt.TabIndex = 19;
+            this.btnSqrt.Text = "√";
+            this.btnSqrt.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(200, 66);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(30, 30);
+            this.btnBack.TabIndex = 20;
+            this.btnBack.Text = "←";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(200, 161);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(30, 30);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "%";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 307);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnSqrt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAmDuong);
+            this.Controls.Add(this.btnThapPhan);
+            this.Controls.Add(this.btnBang);
+            this.Controls.Add(this.btnChia);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.lblDisplay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button btnChia;
+        private System.Windows.Forms.Button btnBang;
+        private System.Windows.Forms.Button btnThapPhan;
+        private System.Windows.Forms.Button btnAmDuong;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSqrt;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Calculator/Calculator/frmMain.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class frmMain : Form
+    {
+        bool isTypingNumber = false;
+        enum PhepToan { Cong, Tru, Nhan, Chia };
+        PhepToan pheptoan;
+        double nho =0.0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        
+//Cac phep toan
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            isTypingNumber = false;
+            pheptoan = PhepToan.Cong;
+            this.btnBang_Click(sender, e);
+            nho = double.Parse(lblDisplay.Text);
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            isTypingNumber = false;
+            pheptoan = PhepToan.Tru;
+            this.btnBang_Click(sender, e);
+            nho = double.Parse(lblDisplay.Text);
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            isTypingNumber = false;
+            pheptoan = PhepToan.Nhan;
+            this.btnBang_Click(sender, e);
+            nho = double.Parse(lblDisplay.Text);
+        }
+
+        private void btnChia_Click(object sender, EventArgs e)
+        {
+            isTypingNumber = false;
+            pheptoan = PhepToan.Chia;
+            this.btnBang_Click(sender, e);
+            nho = double.Parse(lblDisplay.Text);
+        }
+
+        private void btnThapPhan_Click(object sender, EventArgs e)
+        {
+            if (isTypingNumber)
+                lblDisplay.Text = lblDisplay.Text + btnThapPhan.Text;
+            else
+            {
+                lblDisplay.Text = btnThapPhan.Text;
+                isTypingNumber = true;
+            }
+
+        }
+
+        private double TinhKetQua()
+        {
+            
+            double ketqua = 0.0;
+            switch (pheptoan)
+            {
+                case PhepToan.Cong:
+                    ketqua = nho + double.Parse(lblDisplay.Text); break;
+                case PhepToan.Tru:
+                    ketqua = nho - double.Parse(lblDisplay.Text); break;
+                case PhepToan.Nhan:
+                    ketqua = nho * double.Parse(lblDisplay.Text); break;
+                case PhepToan.Chia:
+                    ketqua = nho / double.Parse(lblDisplay.Text); break;
+            }
+            lblDisplay.Text = ketqua.ToString();
+            return ketqua; 
+        }
+
+        private void btnBang_Click(object sender, EventArgs e)
+        {
+
+            isTypingNumber = false;
+            TinhKetQua();
+        }
+
+        private void NhapSo(object sender, EventArgs e)
+        {
+            if (isTypingNumber)
+                lblDisplay.Text = lblDisplay.Text + ((Button)sender).Text;
+            else
+            {
+                lblDisplay.Text = ((Button)sender).Text;
+                isTypingNumber = true;
+            }
+        }
+
+        private void NhapPhepToan(object sender, EventArgs e)
+        {
+            isTypingNumber = false;
+            switch (((Button)sender).Text)
+            {
+                case "+":
+                    pheptoan=PhepToan.Cong;break;
+                case "-":
+                    pheptoan = PhepToan.Tru;break;
+                case "*":
+                    pheptoan = PhepToan.Nhan;break;
+                case "/":
+                    pheptoan = PhepToan.Chia;break;
+
+            }
+
+            TinhKetQua();
+            nho = double.Parse(lblDisplay.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+     
+    }
+}
