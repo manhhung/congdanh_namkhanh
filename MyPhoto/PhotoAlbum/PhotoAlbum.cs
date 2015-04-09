f39135d2dbb56c9edbb4c0be381383ab9462b674
danhkhanh <ronwindy@gmail.com>
2015-04-09 23:10:00 +0700

    bai myphot chuong 6

---
+++ MyPhoto/PhotoAlbum/PhotoAlbum.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace PhotoAlbum
+{
+    class PhotoAlbum: Collection<Photo>
+    {
+        private bool hasChanged = false;
+        public bool HasChanged
+        {
+            get {
+                if (hasChanged) return true;
+                foreach (Photo p in this)
+                    if (p.HasChanged) return true;
+                return false;
+            }
+
+            set {
+                hasChanged = value;
+                if (value == false)
+                    foreach (Photo p in this)
+                        p.HasChanged = false;
+            }
+        }
+
+        public Photo Add(string fileName)
+        {
+            Photo p = new Photo(fileName);
+            base.Add(p);
+            return p;
+        }
+
+        protected override void ClearItems()
+        {
+            if (Count > 0)
+            {
+                base.ClearItems();
+                HasChanged = true;
+            }
+        }
+
+        protected override void InsertItem(int index, Photo item)
+        {
+            HasChanged = true;
+            base.InsertItem(index, item);
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            //Items[index].Dispose();
+            hasChanged = true;
+            base.RemoveItem(index);
+        }
+
+        protected override void SetItem(int index, Photo item)
+        {
+            HasChanged = true;
+            base.SetItem(index, item);
+        }
+    }
+}
