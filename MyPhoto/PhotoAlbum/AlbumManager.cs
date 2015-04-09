63112712e6af943c312d3d4d152d2a7415616085
danhkhanh <ronwindy@gmail.com>
2015-04-09 22:40:47 +0700

    myphoto 3

---
+++ MyPhoto/PhotoAlbum/AlbumManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+
+namespace PhotoAlbum
+{
+    public class AlbumManager
+    {
+        static private string defaultPath;
+        static public string DefaultPath
+        {
+            get { return defaultPath; }
+            set { defaultPath = value; }
+        }
+
+        static AlbumManager()
+        {
+            defaultPath = Environment.GetFolderPath(
+            Environment.SpecialFolder.Personal)
+            + @"\Albums";
+        }
+
+        private int pos = -1;
+        private string name = String.Empty;
+        private PhotoAlbum album;
+
+        public AlbumManager()
+        {
+            album = new PhotoAlbum();
+        }
+
+        public AlbumManager(string name)
+            : this()
+        {
+            name = name;
+            // TODO: load the album
+            throw new NotImplementedException();
+        }
+
+        public PhotoAlbum Album
+        {
+            get { return album; }
+        }
+        public string FullName
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public string ShortName
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(FullName))
+                    return null;
+                else
+                    return Path.
+                     GetFileNameWithoutExtension(FullName);
+            }
+        }
+
+        public Photo Current
+        {
+            get
+            {
+                if (Index < 0 || Index >= Album.Count)
+                    return null;
+                return Album[pos];
+            }
+        }
+    }
+}
---
+++ MyPhoto/PhotoAlbum/Photo.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace PhotoAlbum
+{
+    class Photo
+    {
+        private string fileName;
+        public string FileName
+        {
+            get { return this.fileName; }
+        }
+
+        private Bitmap bitmap;
+        public Bitmap Image
+        {
+            get
+            {
+                if (bitmap == null)
+                    bitmap = new Bitmap(fileName);
+                return bitmap;
+            }
+        }
+
+        private string caption = "";
+        public string Caption
+        {
+            get { return caption; }
+            set
+            {
+                if (caption != value)
+                {
+                    caption = value;
+                    HasChanged = true;
+                }
+            }
+        }
+        private string photographer = "";
+        public string Photographer
+        {
+            get { return photographer; }
+            set
+            {
+                if (photographer != value)
+                {
+                    photographer = value;
+                    HasChanged = true;
+                }
+            }
+        }
+        private DateTime dateTaken = DateTime.Now;
+        public DateTime DateTaken
+        {
+            get
+            {
+                return dateTaken;}
+            set 
+            {
+                if (dateTaken !=value)
+                {dateTaken=value;
+                HasChanged=true;
+            }
+        }
+        }
+        private string notes = "";
+    
+        public string Notes
+        {
+            get { return notes; }
+            set
+            {
+                if (notes != value)
+                {
+                    notes = value;
+                    HasChanged = true;
+                }
+            }
+        }
+
+        private bool hasChanged = true;
+        
+        public Photo(string fileName)
+        {
+            this.fileName = fileName;
+            bitmap = null;
+            caption = System.IO.Path.GetFileNameWithoutExtension(fileName);
+
+        }
+        public bool HasChanged
+        {
+            get { return hasChanged; }
+            set { hasChanged = value; }
+        }
+
+        public override string ToString()
+        {
+            return FileName;
+        }
+
+        public override int GetHashCode()
+        {
+        return ToString().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            
+            return GetHashCode() == obj.GetHashCode();
+        }
+
+
+    }
+}
